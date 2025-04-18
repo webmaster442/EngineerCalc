ed623eb1ab8e57407109103afe1f0aa4e32e57a7
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-04-18 14:03:28 +0200

    Si Prefixes as variables

--- DynamicEvaluator/Variables.cs
+++ DynamicEvaluator/Variables.cs
@@ -14,6 +14,28 @@ public sealed class Variables : IEnumerable<KeyValuePair<string, object>>
             { "e", Math.E },
             { "pi", Math.PI },
             { "tau", Math.Tau },
+            { "yocto", 1e-24 },
+            { "zepto", 1e-21 },
+            { "atto",  1e-18 },
+            { "femto", 1e-15 },
+            { "pico",  1e-12 },
+            { "nano",  1e-9  },
+            { "micro", 1e-6  },
+            { "milli", 1e-3  },
+            { "centi", 1e-2  },
+            { "deci",  1e-1  },
+            { "deca",  1e1   },
+            { "hecto", 1e2   },
+            { "kilo",  1e3   },
+            { "mega",  1e6   },
+            { "giga",  1e9   },
+            { "tera",  1e12  },
+            { "peta",  1e15  },
+            { "exa",   1e18  },
+            { "zetta", 1e21  },
+            { "yotta", 1e24  },
+            { "ronna", 1e27  },
+            { "quetta",1e30  }
         };
     }
 
