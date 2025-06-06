b96eb31c129fcc8de65caa323148eb1faeef2a4f
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-06-06 14:04:42 +0200

    Variable name Latex display completed

--- DynamicEvaluator/Expressions/Specific/VariableExpression.cs
+++ DynamicEvaluator/Expressions/Specific/VariableExpression.cs
@@ -37,7 +37,29 @@ internal sealed class VariableExpression : IExpression
     {
         return identifier switch
         {
+            "alpha" => "\\alpha",
+            "beta" => "\\beta",
+            "gamma" => "\\gamma",
+            "delta" => "\\delta",
+            "epsilon" => "\\epsilon",
+            "zeta" => "\\zeta",
+            "eta" => "\\eta",
+            "theta" => "\\theta",
+            "iota" => "\\iota",
+            "kappa" => "\\kappa",
+            "lambda" => "\\lambda",
+            "mu" => "\\mu",
+            "nu" => "\\nu",
+            "xi" => "\\xi",
             "pi" => "\\pi",
+            "rho" => "\\rho",
+            "sigma" => "\\sigma",
+            "tau" => "\\tau",
+            "upsilon" => "\\upsilon",
+            "phi" => "\\phi",
+            "chi" => "\\chi",
+            "psi" => "\\psi",
+            "omega" => "\\omega",
             _ => identifier,
         };
     }
