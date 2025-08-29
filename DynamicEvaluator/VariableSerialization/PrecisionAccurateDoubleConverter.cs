722ed2d7ddd747987ccfed75524b5a738ee4442f
webmaster442 <ruzsinszki.gabor@gmail.com>
2025-08-29 13:45:27 +0200

    Variable serialization WIP

---
+++ DynamicEvaluator/VariableSerialization/PrecisionAccurateDoubleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DynamicEvaluator.VariableSerialization;
+
+internal sealed class PrecisionAccurateDoubleConverter : JsonConverter<double>
+{
+    public override double Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string value = reader.GetString() 
+            ?? throw new InvalidOperationException("Value can't be null or empty");
+
+        return double.TryParse(value, CultureInfo.InvariantCulture, out double result)
+            ? result
+            : throw new InvalidOperationException($"Value '{value}' is not a valid double.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, double value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("G17", CultureInfo.InvariantCulture));
+    }
+}
