8e050029e0c42f014c7b662708555aa9fc611993
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-05-10 18:14:45 +0200

    Hyperbolic functions

---
+++ DynamicEvaluator.TypeSystem/Internals/ComplexMath.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace DynamicEvaluator.TypeSystem.Internals;
+
+internal static class ComplexMath
+{
+    public static Complex Asinh(Complex complex)
+        => Complex.Log(complex + Complex.Sqrt((complex * complex) + 1));
+
+    public static Complex Acosh(Complex complex)
+        => Complex.Log(complex + Complex.Sqrt((complex * complex) - 1));
+
+    public static Complex Atanh(Complex complex)
+        => Complex.Log((1 + complex) / (1 - complex)) / 2;
+}
--- DynamicEvaluator/Expressions/Specific/SpecialFunctions/CoshExpression.cs
+++ DynamicEvaluator/Expressions/Specific/SpecialFunctions/CoshExpression.cs
@@ -7,14 +7,13 @@ using DynamicEvaluator.TypeSystem;
 
 namespace DynamicEvaluator.Expressions.Specific.SpecialFunctions;
 
-internal sealed class SinExpression : UnaryExpression
+internal sealed class CoshExpression : UnaryExpression
 {
-    public SinExpression(IExpression child) : base(child)
+    public CoshExpression(IExpression child) : base(child)
     {
     }
-
     public override IExpression Differentiate(string byVariable)
-        => new MultiplyExpression(new CosExpression(Child), Child.Differentiate(byVariable));
+        => new MultiplyExpression(new SinhExpression(Child), Child.Differentiate(byVariable));
 
     public override IExpression Simplify()
     {
@@ -24,22 +23,16 @@ internal sealed class SinExpression : UnaryExpression
             // child is constant
             return new ConstantExpression(Evaluate(childConst.Value));
         }
-        if (newChild.IsIntegerMultupleOfPi())
-        {
-            return new ConstantExpression(0L);
-        }
-        return new SinExpression(newChild);
+        return new CoshExpression(newChild);
     }
 
-
-
     protected override Result Evaluate(Result value)
-        => TypeFunctions.Sin(value);
+        => TypeFunctions.Cosh(value);
 
     protected override string Render(bool emitLatex)
     {
         return emitLatex
-            ? $"{{ sin({Child.ToLatex()}) }}"
-            : $"sin({Child})";
+            ? $"{{ cosh({Child.ToLatex()}) }}"
+            : $"cosh({Child})";
     }
 }
